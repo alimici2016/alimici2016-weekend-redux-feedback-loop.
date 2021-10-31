e58a466475fba7eb6e72b9f3622138bfe64ce0ad
Hamsa Alimci <alimici2016@gmail.com>
2021-10-31 14:23:51 -0500

    A good time to commit, I just finished rendering the data to the dom and then I put it in a table

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,9 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function Admin() {
 
-    const [data, setData] = useState({})
+    const [data, setData] = useState([])
 
 
     const fetchData = () => {
@@ -18,20 +25,42 @@ function Admin() {
         })
     };
 
-    // const dataMap = () => {data.map((data, index) => 
-    //     <li key={index}>{data} </li>
-    //     )}
 
     useEffect(() => {
         fetchData();
     }, [])
 
+    console.log(data)?
+
     return (
-            <h2>Inputs</h2>
-           
+        <>
+            <TableContainer component={Paper}>
+                <h2>Inputs</h2>
+                <Table className="dataTable">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Feeling:</TableCell>
+                            <TableCell>Understanding:</TableCell>
+                            <TableCell>Support:</TableCell>
+                            <TableCell>Comments:</TableCell>
+
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {data.map((data) => (
+                            <TableRow>
+                                <TableCell>{data.feeling}</TableCell>
+                                <TableCell>{data.understanding}</TableCell>
+                                <TableCell>{data.support}</TableCell>
+                                <TableCell>{data.comments}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
     );
-
-
 }
 
 export default Admin;
